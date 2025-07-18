5a6d1d5bda54d284699c2fedc31d23f7f12fe589
lautaro noguera <lautaroestudio2023@gmail.com>
2025-07-17 22:05:23 -0300

    agreagada dependdencia de lombok y organizado los archivos un poco

--- src/main/java/techlab/service/DemoApplication.java
+++ src/main/java/techlab/service/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package techlab.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/techlab/service/DemoApplicationTests.java
+++ src/test/java/techlab/service/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package techlab.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
